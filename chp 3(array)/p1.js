42ddfcefa9e0c16bfb3a9e945a60835559d1d212
Shubhramjeet Sahoo <156273873+shubhramongit@users.noreply.github.com>
2025-12-23 15:33:25 +0530

    Delete chp 3(array) directory

--- chp 3(array)/p1.js
+++
@@ -1,9 +0,0 @@
-//objects
-//key value pair
-//dot notation
-let studentobj=new Object();
-console.log(studentobj)
-studentobj.myname="sourav";
-studentobj.age=23;
-studentobj.job="developer";
-console.log(studentobj)
